ad944f2058c48aacc3b5d3ec4a6053cfbfe52393
Aziz_Fayziev <fayziev978@mail.ru>
2025-10-21 13:34:30 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 # .env fayldan ma'lumotlarni yuklaymiz
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OPENAI_KEY = os.getenv("OPENAI_API_KEY")
+BOT_TOKEN = os.getenv("8358036357:AAFCauZkwF4FiS7sbpOAVIjbYHMk1f5hfLo")
+OPENAI_KEY = os.getenv(" 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")
 
 if not OPENAI_KEY:
     raise ValueError("❌ OPENAI_API_KEY topilmadi. Iltimos .env faylni tekshiring!")
